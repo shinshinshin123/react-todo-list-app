650d0862416265b09f53b4b54f362d8249d5c9aa
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2022-12-30 18:10:13 +0900

    ステータスのセレクトボックスを追加する

--- todo_list/src/App.jsx
+++ todo_list/src/App.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import { useState } from "react";
 
 export const App = () => {
-    const [todoValue, setTodoValue] = useState('')
-
     //todoの追加
+    const [todoValue, setTodoValue] = useState('');
+    // const [todoContent, setTodoContent] = useState('');
+
     const [addTodos, setAddTodos] = useState(['']);
+    // const [addContent, setAddContent] = useState(['']);
 
     const onChangeTodoValue = (e) => setTodoValue(e.target.value);
+    // const onChangetodoContent = (e) => setTodoContent(e.target.value);
 
     const onClickAdd = () => {
         if (todoValue === "") return;
         const createTodos = [...addTodos, todoValue];
+        // const createContent = [...addContent, todoContent]
         setAddTodos(createTodos);
+        // setAddContent(createContent);
         setTodoValue('');
+        // setTodoContent('');
     };
 
     //todoの削除
@@ -23,27 +29,20 @@ export const App = () => {
         setAddTodos(createTodos);
     };
 
-    //todoの詳細
-    // const [todoDetail, setTodoDetail] = useState(['']);
+    //ステータスのセレクトボックス
+    const [status, setStatus] = React.useState('react');
 
-    //todoのステータスに関して(未完了、完了、進行中)
-    // const onClickSwitch = (index) => {
-    //   const switchTodo = [...todoValue];
-    //   if (switchTodo[index].status === "進行中") {
-    //     switchTodo[index].status = "完了";
-    //   } else if (switchTodo[index].status === "完了") {
-    //     switchTodo[index].status = "進行中"
-    //   }
-    //   setTodoValue(switchTodo);
-    // };
+    const statusChange = e => setStatus(e.target.value);
 
     return (
     <>
       <div className="todo-area">
         <h1>TODOリスト</h1>
+        <p>タイトル</p>
         <input placeholder="TODOを入力する" value={todoValue} onChange={onChangeTodoValue} />
-        {/* <textarea placeholder="詳細を入力する" value={todoDetail} onChange={onChangeTodoValue} /> */}
-        <button onClick={onClickAdd}>追加</button>
+        {/* <p>詳細</p> */}
+        {/* <textarea placeholder="TODO詳細" value={todoContent} onChange={onChangetodoContent} /> */}
+        <td><button onClick={onClickAdd}>追加</button></td>
       </div>
       <div className="not-complete-area">
         <h2>todo一覧</h2>
@@ -60,8 +59,15 @@ export const App = () => {
                 <tr>
                   <td>{index+1}</td>
                   <td>{todo}</td>
-                  {/* <td><button onClick={() => onClickSwitch(index)}>{todo.status}</button></td> */}
+                  <td>
+                    <select value={status} onChange={statusChange}>
+                      <option value="not-started-yet">未着手</option>
+                      <option value="in-progress">進行中</option>
+                      <option value="completion">完了</option>
+                    </select>
+                  </td>
                   <td><button onClick={() => onClickDelete(index)}>削除</button></td>
+                  <td></td>
                </tr>
             ))}
           </tbody>
