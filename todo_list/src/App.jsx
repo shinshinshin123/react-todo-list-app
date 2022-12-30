953b0c62a615c7a6f2aa74508bccb6b5e3cbe4cf
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2022-12-30 23:14:25 +0900

    詳細機能追加

--- todo_list/src/App.jsx
+++ todo_list/src/App.jsx
@@ -4,22 +4,22 @@ import { useState } from "react";
 export const App = () => {
     //todoの追加
     const [todoValue, setTodoValue] = useState('');
-    // const [todoContent, setTodoContent] = useState('');
+    const [todoContent, setTodoContent] = useState('');
 
     const [addTodos, setAddTodos] = useState(['']);
-    // const [addContent, setAddContent] = useState(['']);
+    const [addContent, setAddContent] = useState(['']);
 
     const onChangeTodoValue = (e) => setTodoValue(e.target.value);
-    // const onChangetodoContent = (e) => setTodoContent(e.target.value);
+    const onChangetodoContent = (e) => setTodoContent(e.target.value);
 
     const onClickAdd = () => {
         if (todoValue === "") return;
         const createTodos = [...addTodos, todoValue];
-        // const createContent = [...addContent, todoContent]
+        const createContent = [...addContent, todoContent]
         setAddTodos(createTodos);
-        // setAddContent(createContent);
+        setAddContent(createContent);
         setTodoValue('');
-        // setTodoContent('');
+        setTodoContent('');
     };
 
     //todoの削除
@@ -40,9 +40,9 @@ export const App = () => {
         <h1>TODOリスト</h1>
         <p>タイトル</p>
         <input placeholder="TODOを入力する" value={todoValue} onChange={onChangeTodoValue} />
-        {/* <p>詳細</p> */}
-        {/* <textarea placeholder="TODO詳細" value={todoContent} onChange={onChangetodoContent} /> */}
-        <td><button onClick={onClickAdd}>追加</button></td>
+        <p>詳細</p>
+        <input placeholder="TODO詳細" value={todoContent} onChange={onChangetodoContent} />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="not-complete-area">
         <h2>todo一覧</h2>
@@ -51,14 +51,16 @@ export const App = () => {
             <tr>
               <td>ID</td>
               <td>タイトル</td>
+              <td>詳細</td>
               <td>状態</td>
             </tr>
           </thead>
           <tbody id="todo-body">
-            {addTodos.map((todo, index) => (
+            {addTodos.map((todo, index, content) => (
                 <tr>
                   <td>{index+1}</td>
                   <td>{todo}</td>
+                  <td>{content}</td>
                   <td>
                     <select value={status} onChange={statusChange}>
                       <option value="not-started-yet">未着手</option>
